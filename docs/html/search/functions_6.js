826c3e88643287bb19269826b96d9fa7a5afc41d
Anna Weber <annaweberdev@gmail.com>
2021-12-17 20:21:38 -0800

    add doxygen docs

---
+++ docs/html/search/functions_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['step_0',['step',['../class_compute_engine.html#ade018b496b236b15d8a26db5cd77b665',1,'ComputeEngine']]]
+];
--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,18 +1,24 @@
 var indexSectionsWithContent =
 {
-  0: "c",
-  1: "c"
+  0: "cgimprsuv",
+  1: "c",
+  2: "cgimprsuv",
+  3: "c"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "pages"
+  1: "classes",
+  2: "functions",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Pages"
+  1: "Classes",
+  2: "Functions",
+  3: "Pages"
 };
 
